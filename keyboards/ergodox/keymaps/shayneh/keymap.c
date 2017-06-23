0f839f804597b342f7cccab9e1f134bd1d2ae14d
Shayne Holmes <github.snth@xoxy.net>
2017-06-23 08:24:03 -0700

    Use built-in process_action() call instead of custom command
    Involves a little plumbing of keyrecord_t through calling functions.

--- keyboards/ergodox/keymaps/shayneh/keymap.c
+++ keyboards/ergodox/keymaps/shayneh/keymap.c
@@ -278,37 +278,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM fn_actions[] = {
 };
 
-void simon_hotkey(keyevent_t event, action_t action)
-{
-    switch (action.kind.id) {
-        /* Key and Mods */
-        case ACT_LMODS:
-        case ACT_RMODS:
-            {
-                uint8_t mods = (action.kind.id == ACT_LMODS) ?  action.key.mods : action.key.mods<<4;
-                if (event.pressed) {
-                    if (mods) {
-                        add_weak_mods(mods);
-                        send_keyboard_report();
-                    }
-                    register_code(action.key.code);
-                } else {
-                    unregister_code(action.key.code);
-                    if (mods) {
-                        del_weak_mods(mods);
-                        send_keyboard_report();
-                    }
-                }
-            }
-            break;
-        default:
-            print("not supported.\n");
-            break;
-    }
-}
-
-void action_plover_key(keyevent_t event) {
-    if (event.pressed) return;
+void action_plover_key(keyrecord_t *record) {
+    if (record->event.pressed) return;
 
     uint8_t savedmods = get_mods();
     uint8_t shift_pressed = (savedmods & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT)));
@@ -328,18 +299,18 @@ void action_plover_key(keyevent_t event) {
     }
 }
 
-uint16_t key_depending_on_mods(keyevent_t event, uint8_t default_key, uint8_t modified_key, uint8_t dependent_mods) {
+uint16_t key_depending_on_mods(keyrecord_t *record, uint8_t default_key, uint8_t modified_key, uint8_t dependent_mods) {
     static bool mod_pressed;
-    if (event.pressed) {
+    if (record->event.pressed) {
         // save mod state that will persist until the unpress
         mod_pressed = (get_mods() & dependent_mods);
     }
     return ACTION_MODS_KEY(0, mod_pressed ? modified_key : default_key);
 }
 
-uint16_t get_any_key_action(keyevent_t event, uint8_t layer) {
-    uint8_t col = event.key.col;
-    uint8_t row = event.key.row;
+uint16_t get_any_key_action(keyrecord_t *record, uint8_t layer) {
+    uint8_t col = record->event.key.col;
+    uint8_t row = record->event.key.row;
 
     switch (layer) {
         case LAYER_BLUESHIFT:
@@ -359,17 +330,17 @@ uint16_t get_any_key_action(keyevent_t event, uint8_t layer) {
             if (col == 4 && row == 12) {
                 return ACTION_MODS_KEY(MOD_LGUI, KC_TAB); // Alt+tab
             } else if (col == 1 && row == 1) { // apostrophe / CMD+`
-                return key_depending_on_mods(event, DV_QUOT, KC_GRV, MOD_BIT(KC_LGUI) | MOD_BIT(KC_RGUI));
+                return key_depending_on_mods(record, DV_QUOT, KC_GRV, MOD_BIT(KC_LGUI) | MOD_BIT(KC_RGUI));
             } else if (col == 1 && row == 13) { // media forward/back
-                return key_depending_on_mods(event, KC_MEDIA_NEXT_TRACK, KC_MEDIA_PREV_TRACK, MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT));
+                return key_depending_on_mods(record, KC_MEDIA_NEXT_TRACK, KC_MEDIA_PREV_TRACK, MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT));
             } else if (col == 0 && row == 0) { // ESC / CMD+`
-                return key_depending_on_mods(event, KC_ESC, KC_GRV, MOD_BIT(KC_LGUI) | MOD_BIT(KC_RGUI));
+                return key_depending_on_mods(record, KC_ESC, KC_GRV, MOD_BIT(KC_LGUI) | MOD_BIT(KC_RGUI));
             }
             break;
         default:
             break;
     }
-    if (!event.pressed) {
+    if (!record->event.pressed) {
         print("Unknown anykey press:\n");
         print("col = "); pdec(col); print("\n");
         print("row = "); pdec(row); print("\n");
@@ -377,17 +348,17 @@ uint16_t get_any_key_action(keyevent_t event, uint8_t layer) {
     return ACTION_NO;
 }
 
-void action_any_key(keyevent_t event, uint8_t layer) {
+void action_any_key(keyrecord_t *record, uint8_t layer) {
     action_t action;
-    action.code = get_any_key_action(event, layer);
+    action.code = get_any_key_action(record, layer);
     if (action.code != ACTION_NO) {
-        simon_hotkey(event, action);
+        process_action(record, action);
     }
 }
 
-uint8_t get_shiftswitch_key(keyevent_t event) {
-    uint8_t col = event.key.col;
-    uint8_t row = event.key.row;
+uint8_t get_shiftswitch_key(keyrecord_t *record) {
+    uint8_t col = record->event.key.col;
+    uint8_t row = record->event.key.row;
 
     if (col == 0) { // Number row
         switch (row) {
@@ -413,8 +384,8 @@ uint8_t get_shiftswitch_key(keyevent_t event) {
     return KC_NO;
 }
 
-void action_shiftswitch(keyevent_t event) {
-    uint8_t keycode = get_shiftswitch_key(event);
+void action_shiftswitch(keyrecord_t *record) {
+    uint8_t keycode = get_shiftswitch_key(record);
     if (keycode == KC_NO) return;
 
     uint8_t savedmods = get_mods();
@@ -427,27 +398,27 @@ void action_shiftswitch(keyevent_t event) {
     if (shift_pressed) {
         del_mods(MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT));
         send_keyboard_report();
-        simon_hotkey(event, action);
+        process_action(record, action);
         set_mods(savedmods);
         send_keyboard_report();
     } else {
-        simon_hotkey(event, action);
+        process_action(record, action);
         del_weak_mods(MOD_BIT(KC_LSFT));
     }
 }
 
-bool is_out_of_order_fkey_release(keyevent_t event, uint32_t layer_state) {
-    if (!event.pressed) return false;
+bool is_out_of_order_fkey_release(keyrecord_t *record, uint32_t layer_state) {
+    if (!record->event.pressed) return false;
 
-    uint8_t row = event.key.row;
+    uint8_t row = record->event.key.row;
     return
         ((row == 0) && (layer_state & 1<<LAYER_NUMPAD)) ||
         ((row == 1) && (layer_state & 1<<LAYER_BLUESHIFT));
 }
 
-void action_fkey(keyevent_t event) {
+void action_fkey(keyrecord_t *record) {
     layer_invert(LAYER_FKEYS);
-    if (is_out_of_order_fkey_release(event, layer_state)) {
+    if (is_out_of_order_fkey_release(record, layer_state)) {
         layer_invert(LAYER_NUMPAD);
         layer_invert(LAYER_BLUESHIFT);
     }
@@ -455,17 +426,15 @@ void action_fkey(keyevent_t event) {
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-    keyevent_t event = record->event;
-
     switch (id) {
         case PLOVER_SWITCH:
-            return action_plover_key(event);
+            return action_plover_key(record);
         case ANY_KEY:
-            return action_any_key(event, opt);
+            return action_any_key(record, opt);
         case SHIFT_SWITCH:
-            return action_shiftswitch(event);
+            return action_shiftswitch(record);
         case FKEY_SWITCH:
-            return action_fkey(event);
+            return action_fkey(record);
         default:
             print("Unknown action_function called\n");
             print("id  = "); phex(id); print("\n");
@@ -476,9 +445,7 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-    keyevent_t event = record->event;
-
-    if (event.pressed) {
+    if (record->event.pressed) {
         switch (id) {
             case PASSWORD1: return MACRO_PASSWORD1;
             case PASSWORD2: return MACRO_PASSWORD2;

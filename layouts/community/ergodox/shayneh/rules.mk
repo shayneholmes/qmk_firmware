fc07275a1aca38e8e98e554e5178dc500046713e
Shayne Holmes <github.snth@xoxy.net>
2018-03-01 14:36:21 -0800

    Fix password fallback
    It actually works now, which is nice.

--- layouts/community/ergodox/shayneh/rules.mk
+++ layouts/community/ergodox/shayneh/rules.mk
@@ -16,6 +16,9 @@ SLEEP_LED_ENABLE := no       # Breathing sleep LED during USB suspend
 UNICODEMAP_ENABLE := no
 UNICODE_ENABLE := no
 
-ifdef $(test -f ./passwords.h && echo "present")
-  DEFINE=-DSHAYNEH_PASSWORDS_H
+ifneq ("$(wildcard $(LAYOUT_KEYMAP_PATH)/passwords.h)","")
+$(info "Found a customized password file.")
+OPT_DEFS+=-DSHAYNEH_PASSWORDS_H
+else
+$(info "No customized password file found; falling back to example file.")
 endif

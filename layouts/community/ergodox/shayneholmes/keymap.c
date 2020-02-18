9081a88de65ff6acb474ed0e40d91bc0337fcd84
Shayne Holmes <github.snth@xoxy.net>
2020-02-18 07:56:16 -0800

    Update light only when layers change
    Lighten up the matrix_scan function.

--- layouts/community/ergodox/shayneholmes/keymap.c
+++ layouts/community/ergodox/shayneholmes/keymap.c
@@ -508,23 +508,27 @@ void matrix_init_user(void)
     ergodox_board_led_off();
 }
 
-LEADER_EXTERNS();
-
 /* override hook */
-void matrix_scan_user(void)
-{
-    uint8_t layer = biton32(layer_state);
+layer_state_t layer_state_set_user(layer_state_t state) {
+    uint8_t highest_layer = biton32(state);
 
-    ergodox_board_led_off();
-    switch (layer) {
+    switch (highest_layer) {
         case 0:
-            // none
+            ergodox_board_led_off();
             break;
         default:
             ergodox_board_led_on();
             break;
     }
 
+    return state;
+}
+
+LEADER_EXTERNS();
+
+/* override hook */
+void matrix_scan_user(void)
+{
     LEADER_DICTIONARY() {
         leading = false;
         leader_end();

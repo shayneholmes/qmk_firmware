056f33bbc061c1fba5042910bcba21424d96fc5e
Shayne Holmes <github.snth@xoxy.net>
2021-12-31 11:19:12 -0800

    Toggle NKRO from movement layer

--- layouts/community/ergodox/shayneholmes/keymap.c
+++ layouts/community/ergodox/shayneholmes/keymap.c
@@ -270,7 +270,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TO_BASE,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,
         RESET,  XXXXXXX,KC_PGUP,KC_UP,  KC_PGDN,XXXXXXX,_______,
         _______,KC_HOME,KC_LEFT,KC_DOWN,KC_RGHT,KC_END,
-        _______,XXXXXXX,XXXXXXX,KC_END, KC_HOME,XXXXXXX,_______,
+        _______,XXXXXXX,XXXXXXX,KC_END, KC_HOME,NK_TOGG,_______,
         _______,_______,_______,MACR(Q),MACR(K),
                                                 _______,_______,
                                                         _______,

804e40c84c7c7fa63c15130b9ab4cd64ddbc7b61
Shayne Holmes <pog.snth@xoxy.net>
2026-09-22 19:44:09 -0700

    Add missing equals sign
    I'm not sure what behavior it had before — it probably did the right
    thing...

--- layouts/community/ergodox/shayneholmes/keymap.c
+++ layouts/community/ergodox/shayneholmes/keymap.c
@@ -163,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,_______,_______
     ),
 
-    [LAYER_HARDWARE_DVORAK] LAYOUT_ergodox(
+    [LAYER_HARDWARE_DVORAK] = LAYOUT_ergodox(
         // left hand
         KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_BSLS,
         KC_TAB, KC_QUOT,KC_COMM,KC_DOT, KC_P,   KC_Y,   LT_MOVE,
